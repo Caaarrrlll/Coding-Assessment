6aa93e710548f40ce1ceaa768e197aef9a125a10
Francois Venter <venter109@gmail.com>
2023-12-10 21:02:03 +0200

    Created all functions for customer editing and removed context from controller

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using TechSolutionsCRM.DBContexts;
 using TechSolutionsCRM.Interfaces;
 using TechSolutionsCRM.Models;
 
@@ -10,12 +8,10 @@ namespace TechSolutionsCRM.Controllers;
 [Route("[controller]")]
 public class CustomersController : Controller
 {
-    private readonly TechSolutionsCRMContext _context;
     readonly ICustomerService _customerService;
 
-    public CustomersController(TechSolutionsCRMContext context, ICustomerService customerService)
+    public CustomersController(ICustomerService customerService)
     {
-        _context = context;
         _customerService = customerService;
     }
 
@@ -50,7 +46,7 @@ public class CustomersController : Controller
     [HttpPost("CreateCustomer")]
     [ProducesResponseType(typeof(Customer), StatusCodes.Status201Created)]
     [ProducesErrorResponseType(typeof(ConflictObjectResult))]
-    public async Task<CreatedAtRouteResult> Create([Bind("Name,Surname,Email,PhoneNumber")] Customer customer)
+    public async Task<ActionResult<Customer>> Create([Bind("Name,Surname,Email,PhoneNumber")] Customer customer)
     {
         Customer? NewCustomer = null;
         try
@@ -69,50 +65,28 @@ public class CustomersController : Controller
     }
 
   
-    // POST: Customers/EditCustomer
     [HttpPatch("EditCustomer")]
-    public async Task<IActionResult> Edit([Bind("Id,Name,Surname,Email,PhoneNumber")] Customer customer)
+    [ProducesResponseType(typeof(Customer), StatusCodes.Status200OK)]
+    public async Task<ActionResult<Customer>> Edit([Bind("Id,Name,Surname,Email,PhoneNumber")] Customer customer)
     { 
         if (ModelState.IsValid)
         {
-            try
+            var editedCustomer = await _customerService.EditCustomer(customer);
+            if(editedCustomer == null)
             {
-                _context.Update(customer);
-                await _context.SaveChangesAsync();
+                return NotFound();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CustomerExists(customer.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        return View(customer);
-    }
 
-    // POST: Customers/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        var customer = await _context.Customer.FindAsync(id);
-        if (customer != null)
-        {
-            _context.Customer.Remove(customer);
+            return CreatedAtRoute(routeName: "CustomerDetails", routeValues: new { id = customer.Id }, value: editedCustomer);
         }
 
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
+        return BadRequest();
     }
 
-    private bool CustomerExists(int id)
+    [HttpDelete("DeleteCustomer")]
+    [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+    public async Task<bool> DeleteCustomer(int id)
     {
-        return _context.Customer.Any(e => e.Id == id);
+        return await _customerService.DeleteCustomer(id);
     }
 }
--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using TechSolutionsCRM.DBContexts;
 using TechSolutionsCRM.Interfaces;
 using TechSolutionsCRM.Models;
@@ -8,11 +9,14 @@ namespace TechSolutionsCRM.Services;
 public class CustomerService : ICustomerService
 {
     protected TechSolutionsCRMContext _context;
+    private readonly IMapper _mapper;
 
-    public CustomerService(TechSolutionsCRMContext context) { 
+    public CustomerService(TechSolutionsCRMContext context, IMapper mapper) { 
         _context = context;
+        _mapper = mapper;
     }
 
+    #region Public Customer Methods
     public async Task<List<Customer>> GetAllCustomers()
     {
         return await _context.Customer.Include(a => a.Addresses).AsNoTracking().ToListAsync();
@@ -27,13 +31,13 @@ public class CustomerService : ICustomerService
             .FirstOrDefaultAsync(cancellationToken);
     }
 
-    public async Task<Customer> CreateCustomer(Customer newCustomer)
+    public async Task<Customer?> CreateCustomer(Customer newCustomer)
     {
         var checkExisting = await _context.Customer.Where(c => c.IdentityNumber == newCustomer.IdentityNumber).FirstOrDefaultAsync();
 
         if (checkExisting != null)
         {
-            throw new Exception("User already Exists");
+            return null;
         }
 
         _context.Customer.Add(newCustomer);
@@ -41,4 +45,39 @@ public class CustomerService : ICustomerService
 
         return await _context.Customer.Where(c => c.IdentityNumber == newCustomer.IdentityNumber).FirstOrDefaultAsync();
     }
+
+    public async Task<Customer?> EditCustomer(Customer UpdatedCustomer)
+    {
+        var ExistingCustomer = await _context.Customer.Where(c => c.Id == UpdatedCustomer.Id).FirstOrDefaultAsync();
+
+        if(ExistingCustomer == null)
+        {
+            return ExistingCustomer;
+        }
+
+        ExistingCustomer = _mapper.Map(UpdatedCustomer, ExistingCustomer);
+
+        _context.Customer.Update(ExistingCustomer);
+        await _context.SaveChangesAsync();
+
+        return await _context.Customer.Where(c => c.Id == UpdatedCustomer.Id).FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> DeleteCustomer(int? id)
+    {
+        var existingCustomer = await _context.Customer.Where(c => c.Id == id).FirstOrDefaultAsync();
+        
+        if (existingCustomer == null)
+        {
+            return true;
+        }
+
+        _context.Customer.Remove(existingCustomer);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+    #endregion
+
+    #region Public Address Methods
+    #endregion
 }
